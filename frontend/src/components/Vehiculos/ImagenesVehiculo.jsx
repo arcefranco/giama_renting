f051d90bbcb7353bbbe0e3bb60ea77cb23496639
franco <arcefranco29@gmail.com>
2025-04-22 11:06:34 -0300

    boton ver imagenes en reporte vehiculos

--- frontend/src/components/Vehiculos/ImagenesVehiculo.jsx
+++ frontend/src/components/Vehiculos/ImagenesVehiculo.jsx
@@ -2,25 +2,28 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import styles from './ImagenesVehiculo.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getModelos } from '../../reducers/Generales/generalesSlice';
 
 const ImagenesVehiculo = () => {
   const { id } = useParams();
   const [imagenes, setImagenes] = useState([]);
   const [vehiculo, setVehiculo] = useState(null);
-
+  const dispatch = useDispatch();
   useEffect(() => {
     Promise.all([
         axios.get(import.meta.env.VITE_REACT_APP_HOST + `vehiculos/getImagenesVehiculos/${id}`).then((res) => setImagenes(res.data)),
         axios.post(import.meta.env.VITE_REACT_APP_HOST + `vehiculos/getVehiculosById`, {id: id}).then((res) => setVehiculo(res.data)),
+        dispatch(getModelos()),
     ])
 
   }, [id]);
-
+  const {modelos} = useSelector(state => state.generalesReducer)
   return (
     <div className={styles.container}>
       {vehiculo && (
-        <h2>
-          {vehiculo[0].dominio} - {vehiculo[0].modelo} - {vehiculo[0].color}
+        <h2 style={{fontSize: "xx-large"}}>
+          {vehiculo[0].dominio} - {modelos.find((m) => m.id === vehiculo[0].modelo)?.nombre } - {vehiculo[0].color}
         </h2>
       )}
 
--- frontend/src/components/Vehiculos/ReporteVehiculos.jsx
+++ frontend/src/components/Vehiculos/ReporteVehiculos.jsx
@@ -7,7 +7,7 @@ import styles from "./ReporteVehiculos.module.css"
 import { locale } from 'devextreme/localization';
 import 'devextreme/dist/css/dx.carmine.css';
 const ReporteVehiculos = () => {
-const dispatch = useDispatch()
+const dispatch = useDispatch();
 useEffect(() => {
     Promise.all([
         dispatch(getVehiculos()),
@@ -26,14 +26,25 @@ const {
 } = useSelector((state) => state.vehiculosReducer)
 const {modelos, proveedoresGPS} = useSelector(state => state.generalesReducer)
 const getNombreModelo = (id) => {
-    const modelo = modelos.find((m) => m.id === id);
-    return modelo ? modelo.nombre : id;
-  };
+  const modelo = modelos.find((m) => m.id === id);
+  return modelo ? modelo.nombre : id;
+};
 
-  const getNombreProveedorGPS = (id) => {
-    console.log(id)
-    const proveedor = proveedoresGPS.find((p) => p.id == id);
-    return proveedor ? proveedor.nombre : '';
+const getNombreProveedorGPS = (id) => {
+  console.log(id)
+  const proveedor = proveedoresGPS.find((p) => p.id == id);
+  return proveedor ? proveedor.nombre : '';
+};
+
+const renderImagenesCell = (data) => {
+    return (
+      <button
+        onClick={() => window.open(`/vehiculos/imagenes/${data.data.id}`, '_blank')}
+        style={{ color: '#1976d2', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
+      >
+        Ver
+      </button>
+    );
   };
 return (
 <div className={styles.container}>
@@ -56,23 +67,25 @@ return (
         <Paging defaultPageSize={10} />
 
         <Column dataField="id" caption="ID" width={50} />
-        <Column dataField="modelo" caption="Modelo" 
+        <Column dataField="modelo" width={75} caption="Modelo" 
         cellRender={({ data }) => getNombreModelo(data.modelo)}/>
-        <Column dataField="fecha_ingreso" caption="Fecha Ingreso" dataType="date" />
-        <Column dataField="precio_inicial" caption="Precio Inicial" format="currency" />
+        <Column dataField="fecha_ingreso" width={85} caption="Ingreso" dataType="date" />
+        <Column dataField="precio_inicial" caption="Precio Inicial" width={100} format="currency" />
         <Column dataField="dominio" caption="Dominio" />
         <Column dataField="nro_chasis" caption="Nro. Chasis" />
         <Column dataField="nro_motor" caption="Nro. Motor" />
-        <Column dataField="kilometros_iniciales" caption="Km Iniciales" format="fixedPoint" />
+        <Column dataField="kilometros_iniciales" width={100} caption="Km Iniciales" format="fixedPoint" />
         <Column
           dataField="proveedor_gps"
           caption="Proveedor GPS"
+          width={100}
           cellRender={({ data }) => getNombreProveedorGPS(data.proveedor_gps)}
         />
         <Column dataField="nro_serie_gps" caption="Serie GPS" />
         <Column dataField="dispositivo_peaje" caption="Peaje" />
-        <Column dataField="meses_amortizacion" caption="Amortización (Meses)" />
-        <Column dataField="color" caption="Color" />
+        <Column dataField="meses_amortizacion" width={75} caption="Amortización (Meses)" />
+        <Column dataField="color" caption="Color" width={75} />
+        <Column dataField="id" caption="Imágenes" width={100} alignment="center" cellRender={renderImagenesCell} />
       </DataGrid>
     </div>
 )
