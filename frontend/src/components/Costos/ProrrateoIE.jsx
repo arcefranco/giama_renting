21ae9dd926784fd59523728c63b5f9647478c08a
franco <arcefranco29@gmail.com>
2025-08-15 16:53:54 -0300

    prorrateo proveedores

--- frontend/src/components/Costos/ProrrateoIE.jsx
+++ frontend/src/components/Costos/ProrrateoIE.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getVehiculos } from '../../reducers/Vehiculos/vehiculosSlice';
 import { reset, prorrateoIE, getConceptosCostos } from '../../reducers/Costos/costosSlice'
 import {getFormasDeCobro} from "../../reducers/Alquileres/alquileresSlice.js"
-import {getModelos} from '../../reducers/Generales/generalesSlice'
+import {getModelos, getProveedores} from '../../reducers/Generales/generalesSlice'
 import { ToastContainer } from 'react-toastify';
 import { ClipLoader } from "react-spinners";
 import styles from './ProrrateoIE.module.css'
@@ -16,10 +16,12 @@ useEffect(() => {
         dispatch(getVehiculos()),
         dispatch(getConceptosCostos()),
         dispatch(getModelos()),
-        dispatch(getFormasDeCobro())
+        dispatch(getFormasDeCobro()),
+        dispatch(getProveedores())
     ])
     
 }, [])
+const {username} = useSelector((state) => state.loginReducer)
 const [form, setForm] = useState({
     arrayVehiculos: [],
     fecha: '',
@@ -31,9 +33,15 @@ const [form, setForm] = useState({
     observacion: '',
     cuenta: '',
     id_forma_cobro: '',
+    cta_cte_proveedores: '',
+    cod_proveedor: '',
+    tipo_comprobante: '',
+    numero_comprobante_1: '',
+    numero_comprobante_2: '',
+    usuario: username
 })
 const { vehiculos } = useSelector((state) => state.vehiculosReducer)
-const { modelos } = useSelector((state) => state.generalesReducer)
+const {modelos, proveedores} = useSelector((state) => state.generalesReducer)
 const {isError, isSuccess, isLoading, message, conceptos} = useSelector((state) => state.costosReducer)
 const {formasDeCobro} = useSelector((state) => state.alquileresReducer)
 const [conceptosFiltrados, setConceptosFiltrados] = useState(null)
@@ -59,6 +67,40 @@ const handleChange = (e) => {
      ...form,
      [name]: value,
      "importe_total": parseFloat(value) + parseFloat(form["importe_neto"])
+   }); 
+  }
+  else if(value && name == "numero_comprobante_1"){
+    if (value.length > 5) value = value.slice(0, 5)
+      setForm({
+     ...form,
+     [name]: value,
+   }); 
+  }
+  else if(value && name == "numero_comprobante_2"){
+    if (value.length > 8) value = value.slice(0, 8)
+      setForm({
+     ...form,
+     [name]: value,
+   }); 
+  }
+  else if(value && name == "cod_proveedor"){
+    setForm({
+     ...form,
+     [name]: value,
+     tipo_comprobante: proveedores?.find(e => e.Codigo == value)?.TipoResponsable == 1 ? 1 : 3,
+      importe_neto: 0,
+     importe_iva: 0,
+     importe_total: 0
+   }); 
+  }
+  else if(value && name == "tipo_comprobante"){
+      setForm({
+     ...form,
+     [name]: value,
+     importe_neto: 0,
+     importe_iva: 0,
+     importe_total: 0
+     
    }); 
   }
   else{
@@ -235,9 +277,63 @@ const todosSeleccionados = () => {
                 onChange={handleChange}/>
               </div>
               <div className={styles.inputContainer}>
-                  <span>Comprobante</span>
-                  <input type="text" name='comprobante' value={form["comprobante"]} 
-                onChange={handleChange}/>
+                <span>Concepto</span>
+                <select name="id_concepto" style={{width: "130%"}}  value={form["id_concepto"]} 
+                onChange={handleChange} id="">
+                  <option value={""} disabled selected>{"Seleccione un concepto"}</option>
+                  {
+                    conceptosFiltrados?.length && conceptosFiltrados?.map(e => {
+                    return <option key={e.id} value={e.id}>{e.id} - {e.nombre}</option>
+                    })
+                  }
+                </select>
+              </div>
+              <div className={styles.inputContainer} style={{flexDirection: "row", width: "9rem", height: "3rem"}}>
+              <label style={{fontSize: "15px"}}>Cuenta corriente proveedores</label>
+              <input
+                type="checkbox"
+                checked={form.cta_cte_proveedores}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    cta_cte_proveedores: e.target.checked ? 1 : 0,
+                  })
+                }
+                />
+              </div>
+               {
+              form.cta_cte_proveedores == 1 &&
+              <div className={styles.inputContainer}>
+                <span>Proveedores</span>
+                <select name="cod_proveedor" style={{width: "130%"}}  value={form["cod_proveedor"]} 
+                onChange={handleChange} id="">
+                  <option value={""} disabled selected>{"Seleccione un proveedor"}</option>
+                  {
+                    proveedores?.length && proveedores?.map(e => {
+                    return <option key={e.Codigo} value={e.Codigo}>{e.RazonSocial}</option>
+                    })
+                  }
+                </select>
+              </div>
+              }
+              <div className={styles.inputContainer}>
+              <span>Tipo comprobante</span>
+              <select name="tipo_comprobante" style={{width: "130%"}}  value={form["tipo_comprobante"]} 
+              onChange={handleChange} id="">
+                <option value={0}></option>
+                <option value={1}>FA</option>
+                <option value={3}>FC</option>
+              </select>
+              </div>
+              <div className={styles.inputContainer}>
+                <span>Punto de venta</span>
+                <input type="number" name='numero_comprobante_1' value={form.numero_comprobante_1}
+                onChange={handleChange} max="99999" />
+              </div>
+              <div className={styles.inputContainer}>
+                <span>Nº Comprobante</span>
+                <input type="number" name='numero_comprobante_2' value={form.numero_comprobante_2}
+                onChange={handleChange} max="99999999" />
               </div>
               <div className={styles.inputContainer}>
                   <span>Importe neto</span>
@@ -255,7 +351,8 @@ const todosSeleccionados = () => {
               </div>
               <div className={styles.inputContainer}>
                 <span>Forma de cobro</span>
-                <select name="id_forma_cobro"  value={form["id_forma_cobro"]} 
+                <select name="id_forma_cobro" disabled={form.cta_cte_proveedores === 1 ? true : false}
+                  value={form["id_forma_cobro"]} 
                 onChange={handleChange} id="">
                   <option value={""} disabled selected>{"Seleccione una opción"}</option>
                   {
@@ -270,18 +367,7 @@ const todosSeleccionados = () => {
                   <textarea type="text" name='observacion' value={form["observacion"]} 
                 onChange={handleChange}/>
               </div>
-              <div className={styles.inputContainer}>
-                <span>Concepto</span>
-                <select name="id_concepto" style={{width: "130%"}}  value={form["id_concepto"]} 
-                onChange={handleChange} id="">
-                  <option value={""} disabled selected>{"Seleccione un concepto"}</option>
-                  {
-                    conceptosFiltrados?.length && conceptosFiltrados?.map(e => {
-                    return <option key={e.id} value={e.id}>{e.id} - {e.nombre}</option>
-                    })
-                  }
-                </select>
-              </div>
+
               </form>
               <button 
               className={styles.sendBtn} 
