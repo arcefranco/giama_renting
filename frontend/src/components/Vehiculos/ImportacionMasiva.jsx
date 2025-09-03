5371773040bcca7fea9d207b58abca8c507bb1fe
franco <arcefranco29@gmail.com>
2025-09-03 17:04:13 -0300

    descarga plantilla de importacion masiva

--- frontend/src/components/Vehiculos/ImportacionMasiva.jsx
+++ frontend/src/components/Vehiculos/ImportacionMasiva.jsx
@@ -5,6 +5,7 @@ import { postVehiculosMasivos, reset } from '../../reducers/Vehiculos/vehiculosS
 import { useToastFeedback } from '../../customHooks/useToastFeedback'
 import { ToastContainer } from 'react-toastify';
 import { ClipLoader } from "react-spinners";
+import downloadicon from "../../assets/downloadicon.svg";
 const ImportacionMasiva = () => {
 const dispatch = useDispatch()
 const {isError, isSuccess, isLoading, message} = useSelector((state) => state.vehiculosReducer)
@@ -36,7 +37,22 @@ const handleSubmit = (e) => {
     <p className={styles.loadingText}>Ingresando vehículos...</p>
   </div>
 )}
-    <h2 style={{margin: "1rem", color: "#800000"}}>Importación masiva de vehículos</h2>
+<div>
+  <div style={{display: "flex", justifyContent: "space-between", width: "90%"}}>
+  <h2 style={{margin: "1rem", color: "#800000"}}>Importación masiva de vehículos</h2>
+  <button 
+  className={styles.sendBtn}
+  style={{width: "8rem", fontSize: "12px", display: "flex", alignItems: "center", justifyContent: "space-evenly"}}
+  onClick={() => window.open("/importacion_masiva_vehiculos_planilla.xlsx", "_blank")}>
+    <img 
+    src={downloadicon} 
+    alt="Descargar" 
+    style={{ width: "19px"}}
+  />
+  Descargar plantilla
+  </button>
+
+  </div>
     <div className={styles.container} style={{justifySelf: "center"}}>
         <div className={styles.inputContainer} style={{width: "16rem", justifyContent: "space-between"}}>
             <span>Ingrese el archivo excel</span>
@@ -44,6 +60,8 @@ const handleSubmit = (e) => {
         </div>
         <button className={styles.sendBtn} onClick={handleSubmit}>Enviar</button>
     </div>
+
+</div>
     </>
   )
 }
