2b0ae256ebb398422d02317b951f6ac8a1e6ff38
franco <arcefranco29@gmail.com>
2025-09-17 13:39:17 -0300

    vite config para testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE || "/",
-});
+export default ({ mode }) => {
+  // Cargar variables del .env según el modo
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_BASE || "/",
+  });
+};
