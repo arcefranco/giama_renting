6ceeae1be4e7448349f1ea474e3692f47b7c52fe
franco <arcefranco29@gmail.com>
2025-09-17 13:19:16 -0300

    basename testing env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ import { locale } from 'devextreme/localization';
 import { loadMessages } from 'devextreme/localization';
 import * as messagesEs from 'devextreme/localization/messages/es.json';
 import { PersistGate } from 'redux-persist/integration/react';
-
+const base = import.meta.env.REACT_APP_BASENAME || "/";
 
 
 loadMessages(messagesEs); // Cargar traducciones a español
@@ -20,7 +20,7 @@ locale('es');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
   <PersistGate loading={null} persistor={persistor}>
-  <BrowserRouter>
+  <BrowserRouter basename={base}>
     <App />
   </BrowserRouter>
   </PersistGate>
