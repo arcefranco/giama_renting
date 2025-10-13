85cf6607be1e3c90a8befe8f5b6301f0b872b66d
franco <arcefranco29@gmail.com>
2025-10-13 12:08:42 -0300

    env base_name en reporte vehiculos

--- frontend/src/components/Vehiculos/ReporteVehiculos.jsx
+++ frontend/src/components/Vehiculos/ReporteVehiculos.jsx
@@ -89,7 +89,7 @@ const ReporteVehiculos = () => {
   const renderImagenesCell = (data) => {
     return (
       <button
-        onClick={() => window.open(`/vehiculos/imagenes/${data.data.id}`, '_blank')}
+        onClick={() => window.open(`${import.meta.env.VITE_BASE_NAME}vehiculos/imagenes/${data.data.id}`, '_blank')}
         style={{ color: '#1976d2', fontSize: "11px", textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
       >
         Archivos
@@ -99,7 +99,7 @@ const ReporteVehiculos = () => {
   const renderModificarCell = (data) => {
     return (
       <button
-        onClick={() => window.open(`/vehiculos/actualizar/${data.data.id}`, '_blank')}
+        onClick={() => window.open(`${import.meta.env.VITE_BASE_NAME}vehiculos/actualizar/${data.data.id}`, '_blank')}
         style={{ color: '#1976d2', fontSize: "11px", textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
       >
         Modificar
@@ -109,7 +109,7 @@ const ReporteVehiculos = () => {
   const renderEgresosCell = (data) => {
     return (
       <button
-        onClick={() => window.open(`/costos/egresos/${data.data.id}`, '_blank')}
+        onClick={() => window.open(`${import.meta.env.VITE_BASE_NAME}costos/egresos/${data.data.id}`, '_blank')}
         style={{ color: '#1976d2', fontSize: "11px", textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
       >
         Egresos
@@ -119,7 +119,7 @@ const ReporteVehiculos = () => {
   const renderIngresosCell = (data) => {
     return (
       <button
-        onClick={() => window.open(`/costos/ingresos/${data.data.id}`, '_blank')}
+        onClick={() => window.open(`${import.meta.env.VITE_BASE_NAME}costos/ingresos/${data.data.id}`, '_blank')}
         style={{ color: '#1976d2', fontSize: "11px", textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
       >
         Ingresos
@@ -129,7 +129,7 @@ const ReporteVehiculos = () => {
   const renderFichaCell = (data) => {
     return (
       <button
-        onClick={() => window.open(`/vehiculos/ficha/${data.data.id}`, '_blank')}
+        onClick={() => window.open(`${import.meta.env.VITE_BASE_NAME}vehiculos/ficha/${data.data.id}`, '_blank')}
         style={{
           color: '#1976d2', fontSize: "11px",
           textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer'
