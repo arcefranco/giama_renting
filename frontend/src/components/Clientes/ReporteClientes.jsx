d8a0d880df49f6756332966d6c662600341c1a6f
franco <arcefranco29@gmail.com>
2025-10-13 12:30:01 -0300

    env variable en botones render de los data grid

--- frontend/src/components/Clientes/ReporteClientes.jsx
+++ frontend/src/components/Clientes/ReporteClientes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import DataGrid, {Column, Scrolling, Export, SearchPanel, FilterRow, HeaderFilter, Paging} from "devextreme-react/data-grid"
+import DataGrid, { Column, Scrolling, Export, SearchPanel, FilterRow, HeaderFilter, Paging } from "devextreme-react/data-grid"
 import { getClientes } from '../../reducers/Clientes/clientesSlice';
 import { getTiposDocumento, getTiposResponsable, getProvincias } from '../../reducers/Generales/generalesSlice.js';
 import styles from "./ReporteClientes.module.css"
@@ -14,132 +14,132 @@ import { ToastContainer } from 'react-toastify';
 import { useToastFeedback } from '../../customHooks/useToastFeedback.jsx';
 import { reset } from '../../reducers/Costos/costosSlice.js';
 const ReporteClientes = () => {
-const dispatch = useDispatch();
-useEffect(() => {
+  const dispatch = useDispatch();
+  useEffect(() => {
     Promise.all([
-        dispatch(getClientes()),
-        dispatch(getTiposResponsable()),
-        dispatch(getTiposDocumento()),
-        dispatch(getProvincias()),
-        locale('es')
+      dispatch(getClientes()),
+      dispatch(getTiposResponsable()),
+      dispatch(getTiposDocumento()),
+      dispatch(getProvincias()),
+      locale('es')
     ])
 
-}, [])
-const {
+  }, [])
+  const {
     clientes,
     message,
     isError,
     isSuccess,
     isLoading
-} = useSelector((state) => state.clientesReducer)
-useToastFeedback({
-  isError,
-  isSuccess,
-  message,
-  resetAction: reset
-})
-const {
+  } = useSelector((state) => state.clientesReducer)
+  useToastFeedback({
+    isError,
+    isSuccess,
+    message,
+    resetAction: reset
+  })
+  const {
     provincias,
     tipos_responsable,
     tipos_documento
-} = useSelector((state) => state.generalesReducer)
+  } = useSelector((state) => state.generalesReducer)
 
-const handleActualizar = ( ) => {
-  dispatch(getClientes())
-}
-const getTipoDocumento = (id) => {
+  const handleActualizar = () => {
+    dispatch(getClientes())
+  }
+  const getTipoDocumento = (id) => {
 
-    const tipoDoc = tipos_documento.find((m) => m.id === id); 
+    const tipoDoc = tipos_documento.find((m) => m.id === id);
     return tipoDoc ? tipoDoc.nombre : id;
-}
+  }
 
-const getTipoResponsableCell = (id) => {
-    const tipoRes = tipos_responsable.find((m) => m.id == id); 
+  const getTipoResponsableCell = (id) => {
+    const tipoRes = tipos_responsable.find((m) => m.id == id);
     console.log(tipos_responsable, id)
     return tipoRes ? tipoRes.nombre : id;
-}
-
-const getProvinciasCell = (id) => {
-    const provincia = provincias.find((m) => m.id === id); 
-    return provincia ? provincia.nombre : id;
-}
-const renderFecha = (data) => {
-  if(data.value){
-    let fechaSplit = data?.value?.split("-")
-    return `${fechaSplit[2]}/${fechaSplit[1]}/${fechaSplit[0]}`
-
   }
-}
-const renderImagenesCell = (data) => {
+
+  const getProvinciasCell = (id) => {
+    const provincia = provincias.find((m) => m.id === id);
+    return provincia ? provincia.nombre : id;
+  }
+  const renderFecha = (data) => {
+    if (data.value) {
+      let fechaSplit = data?.value?.split("-")
+      return `${fechaSplit[2]}/${fechaSplit[1]}/${fechaSplit[0]}`
+
+    }
+  }
+  const renderImagenesCell = (data) => {
     return (
       <button
-        onClick={() => window.open(`/clientes/imagenes/${data.data.id}`, '_blank')}
+        onClick={() => window.open(`${import.meta.env.VITE_BASENAME}clientes/imagenes/${data.data.id}`, '_blank')}
         style={{ color: '#1976d2', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
       >
         Ver imágenes
       </button>
     );
-};
-const renderModificarCell = (data) => {
+  };
+  const renderModificarCell = (data) => {
     return (
       <button
-        onClick={() => window.open(`/clientes/actualizar/${data.data.id}`, '_blank')}
+        onClick={() => window.open(`${import.meta.env.VITE_BASENAME}clientes/actualizar/${data.data.id}`, '_blank')}
         style={{ color: '#1976d2', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
       >
         Modificar
       </button>
     );
-};
-const renderImprimirCell = (data) => {
-  return (
-    <button
-      onClick={() => window.open(`/clientes/actualizar/${data.data.id}?imprimir=true`, '_blank')}
-      style={{ color: 'green', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
-    >
-      🖨️ Imprimir
-    </button>
-  );
-};
-const renderResolucion = ({ data }) => {
-  const valor = data.resolucion_datero;
-
-  const opciones = {
-    0: { label: 'Pendiente de aprobación', icon: sinResolucionIcon },
-    1: { label: 'Aprobado', icon: aprobadoIcon },
-    2: { label: 'Rechazado', icon: rechazadoIcon }
   };
+  const renderImprimirCell = (data) => {
+    return (
+      <button
+        onClick={() => window.open(`${import.meta.env.VITE_BASENAME}clientes/actualizar/${data.data.id}?imprimir=true`, '_blank')}
+        style={{ color: 'green', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
+      >
+        🖨️ Imprimir
+      </button>
+    );
+  };
+  const renderResolucion = ({ data }) => {
+    const valor = data.resolucion_datero;
 
-  const seleccion = opciones[valor] || { label: 'Desconocido', icon: sinResolucionIcon };
+    const opciones = {
+      0: { label: 'Pendiente de aprobación', icon: sinResolucionIcon },
+      1: { label: 'Aprobado', icon: aprobadoIcon },
+      2: { label: 'Rechazado', icon: rechazadoIcon }
+    };
 
+    const seleccion = opciones[valor] || { label: 'Desconocido', icon: sinResolucionIcon };
+
+    return (
+      <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+        <img src={seleccion.icon} alt={seleccion.label} style={{ width: 20, height: 20 }} />
+        <span>{seleccion.label}</span>
+      </div>
+    );
+  };
   return (
-    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-      <img src={seleccion.icon} alt={seleccion.label} style={{ width: 20, height: 20 }} />
-      <span>{seleccion.label}</span>
-    </div>
-  );
-};
-return (
-<div className={styles.container}>
-<ToastContainer /> 
-{isLoading && (
-    <div className={styles.spinnerOverlay}>
-    <ClipLoader
-      size={60}
-      color="#800020" // bordó
-      loading={true}
-    />
-      <span className={styles.loadingText}>Cargando clientes...</span>
-    </div>
-  )}
-    <h2>Reporte de clientes ingresados</h2>
-    <button onClick={handleActualizar} className={styles.refreshButton}>
-    🔄 Actualizar reporte
-    </button>
+    <div className={styles.container}>
+      <ToastContainer />
+      {isLoading && (
+        <div className={styles.spinnerOverlay}>
+          <ClipLoader
+            size={60}
+            color="#800020" // bordó
+            loading={true}
+          />
+          <span className={styles.loadingText}>Cargando clientes...</span>
+        </div>
+      )}
+      <h2>Reporte de clientes ingresados</h2>
+      <button onClick={handleActualizar} className={styles.refreshButton}>
+        🔄 Actualizar reporte
+      </button>
       <DataGrid
         className={styles.dataGrid}
         dataSource={clientes || []}
         showBorders={true}
-        style={{fontFamily: "IBM"}}
+        style={{ fontFamily: "IBM" }}
         rowAlternationEnabled={true}
         allowColumnResizing={true}
         columnAutoWidth={true}
@@ -151,53 +151,53 @@ return (
         <Scrolling mode="standard" />
         <Paging defaultPageSize={10} />
         <Column dataField="id" caption="ID" width={60} />
-        <Column dataField="nombre" caption="Nombre" width={110}/>
-        <Column dataField="apellido" caption="Apellido" width={85}/>
+        <Column dataField="nombre" caption="Nombre" width={110} />
+        <Column dataField="apellido" caption="Apellido" width={85} />
         <Column dataField="razon_social" caption="Razón social" width={85} />
-        <Column dataField="fecha_nacimiento" alignment="center" 
-        cellRender={renderFecha}
-        caption="Nacimiento" width={110}/>
+        <Column dataField="fecha_nacimiento" alignment="center"
+          cellRender={renderFecha}
+          caption="Nacimiento" width={110} />
         <Column dataField="nacionalidad" caption="Nacionalidad" width={90} />
         <Column dataField="tipo_contribuyente" caption="Tipo responsable"
-        cellRender={({ data }) => getTipoResponsableCell(data["tipo_contribuyente"])}
-        width={140}/>
-        <Column dataField="tipo_documento" 
-        cellRender={({ data }) => getTipoDocumento(data["tipo_documento"])}
-        caption="Tipo documento" width={90} />
+          cellRender={({ data }) => getTipoResponsableCell(data["tipo_contribuyente"])}
+          width={140} />
+        <Column dataField="tipo_documento"
+          cellRender={({ data }) => getTipoDocumento(data["tipo_documento"])}
+          caption="Tipo documento" width={90} />
         <Column dataField="nro_documento" caption="Nro. documento" width={90} />
-        <Column dataField="doc_expedido_por" caption="Doc. exp. por" width={90}/>
-        <Column dataField="licencia" caption="Licencia" width={90}/>
-        <Column dataField="lic_expedida_por" caption="Lic. exp. por" width={90}/>
+        <Column dataField="doc_expedido_por" caption="Doc. exp. por" width={90} />
+        <Column dataField="licencia" caption="Licencia" width={90} />
+        <Column dataField="lic_expedida_por" caption="Lic. exp. por" width={90} />
         <Column dataField="fecha_vencimiento_licencia"
-        cellRender={renderFecha} alignment="center"
-        caption="Vencimiento (licencia)" width={120} />
-        <Column dataField="direccion" caption="Dirección" width={90}/>
-        <Column dataField="nro_direccion" caption="Nro. dirección" width={90}/>
-        <Column dataField="piso" caption="Piso" width={60}/>
-        <Column dataField="depto" caption="Depto." width={60}/>
-        <Column dataField="codigo_postal" caption="Código postal" width={90}/>
+          cellRender={renderFecha} alignment="center"
+          caption="Vencimiento (licencia)" width={120} />
+        <Column dataField="direccion" caption="Dirección" width={90} />
+        <Column dataField="nro_direccion" caption="Nro. dirección" width={90} />
+        <Column dataField="piso" caption="Piso" width={60} />
+        <Column dataField="depto" caption="Depto." width={60} />
+        <Column dataField="codigo_postal" caption="Código postal" width={90} />
         <Column dataField="provincia" caption="Provincia"
-        cellRender={({ data }) => getProvinciasCell(data["provincia"])} 
-        width={100}/>
-        <Column dataField="ciudad" caption="Ciudad" width={110}/>
-        <Column dataField="celular" caption="Celular" width={90}/>
-        <Column dataField="telefono_alternativo" caption="Tél. alternativo" width={90}/>
-        <Column dataField="mail" caption="Mail" width={90}/>
+          cellRender={({ data }) => getProvinciasCell(data["provincia"])}
+          width={100} />
+        <Column dataField="ciudad" caption="Ciudad" width={110} />
+        <Column dataField="celular" caption="Celular" width={90} />
+        <Column dataField="telefono_alternativo" caption="Tél. alternativo" width={90} />
+        <Column dataField="mail" caption="Mail" width={90} />
         <Column dataField="notas" caption="Notas" width={200} />
-        <Column dataField="resolucion_datero" caption="Res. datero" width={200} cellRender={renderResolucion}/>
+        <Column dataField="resolucion_datero" caption="Res. datero" width={200} cellRender={renderResolucion} />
         <Column dataField="usuario_alta" caption="Usuario alta" width={200} />
         <Column dataField="id" caption="" width={100} alignment="center" cellRender={renderImagenesCell} />
-        <Column dataField="id"  width={100} caption="" alignment="center" cellRender={renderModificarCell} />
+        <Column dataField="id" width={100} caption="" alignment="center" cellRender={renderModificarCell} />
         <Column
-        dataField="id"
-        caption="Imprimir"
-        width={100}
-        alignment="center"
-        cellRender={renderImprimirCell}
+          dataField="id"
+          caption="Imprimir"
+          width={100}
+          alignment="center"
+          cellRender={renderImprimirCell}
         />
       </DataGrid>
-</div>
-)
+    </div>
+  )
 }
 
 export default ReporteClientes
--- frontend/src/components/Parametros/Sucursales/Sucursales.jsx
+++ frontend/src/components/Parametros/Sucursales/Sucursales.jsx
@@ -1,115 +1,115 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { ToastContainer} from 'react-toastify';
-import {getSucursales} from "../../../reducers/Generales/generalesSlice.js"
+import { ToastContainer } from 'react-toastify';
+import { getSucursales } from "../../../reducers/Generales/generalesSlice.js"
 import { postSucursal, reset } from '../../../reducers/Parametros/parametrosSlice.js';
 import { ClipLoader } from "react-spinners";
 import styles from "./../Parametros.module.css"
 import { useToastFeedback } from '../../../customHooks/useToastFeedback.jsx';
-import DataGrid, {Column} from "devextreme-react/data-grid"
+import DataGrid, { Column } from "devextreme-react/data-grid"
 import Swal from 'sweetalert2';
 
 const Sucursales = () => {
-const dispatch = useDispatch()
-const {isError, isSuccess, isLoading, message} = useSelector((state) => state.parametrosReducer)
-const {isLoading: isLoadingSucursales} = useSelector((state) => state.generalesReducer)
-const {sucursales} = useSelector((state) => state.generalesReducer)
-const [form, setForm] = useState({
-    nombre: '',
-})
-useEffect(() => {
-Promise.all([
-    dispatch(getSucursales()),
-])
-}, [])
-
-useToastFeedback({
-  isError, 
-  isSuccess,
-  message,
-  resetAction: reset,
-  onSuccess: () => {  
-  dispatch(reset())
-  dispatch(getSucursales())
-  setForm({
+  const dispatch = useDispatch()
+  const { isError, isSuccess, isLoading, message } = useSelector((state) => state.parametrosReducer)
+  const { isLoading: isLoadingSucursales } = useSelector((state) => state.generalesReducer)
+  const { sucursales } = useSelector((state) => state.generalesReducer)
+  const [form, setForm] = useState({
     nombre: '',
   })
+  useEffect(() => {
+    Promise.all([
+      dispatch(getSucursales()),
+    ])
+  }, [])
+
+  useToastFeedback({
+    isError,
+    isSuccess,
+    message,
+    resetAction: reset,
+    onSuccess: () => {
+      dispatch(reset())
+      dispatch(getSucursales())
+      setForm({
+        nombre: '',
+      })
+    }
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
   }
-})
 
-const handleChange = (e) => {
-const { name, value } = e.target;
+  const handleActualizar = () => {
+    dispatch(getSucursales())
+  }
 
-setForm({
-    ...form,
-    [name]: value,
-});
-}
-
-const handleActualizar = () => {
-  dispatch(getSucursales())
-}
-
-const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     dispatch(postSucursal(form))
-} 
+  }
 
-const renderModificarCell = (data) => {
+  const renderModificarCell = (data) => {
     return (
       <button
-        onClick={() => window.open(`/parametros/sucursales/${data.data.id}`, '_blank')}
+        onClick={() => window.open(`${import.meta.env.VITE_BASENAME}parametros/sucursales/${data.data.id}`, '_blank')}
         style={{ color: '#1976d2', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
       >
         Modificar
       </button>
     );
-};
+  };
 
 
   return (
     <div>
       <div className={styles.container}>
-          <ToastContainer /> 
-          {(isLoading || isLoadingSucursales) && (
-      <div className={styles.spinnerOverlay}>
-        <ClipLoader
-          size={60}
-          color="#800020" // bordó
-          loading={true}
-        />
-        <p className={styles.loadingText}>Cargando...</p>
-      </div>
-    )}
-    <h2>Sucursales</h2>
-      <button onClick={handleActualizar} className={styles.refreshButton}>
-      🔄 Actualizar
-      </button>
-      <DataGrid
-        className={styles.dataGrid}
-        dataSource={sucursales || []}
-        showBorders={true}
-        style={{fontFamily: "IBM"}}
-        rowAlternationEnabled={true}
-        allowColumnResizing={true}
-        height={300}
-        columnAutoWidth={true}>
-        <Column dataField="nombre" caption="Nombre"/>
-        <Column dataField="id"  width={100} caption="" alignment="center" cellRender={renderModificarCell} />
-      </DataGrid>
-            <h2>Alta de sucursal</h2>
-            <form action="" enctype="multipart/form-data" className={styles.form}>
-            <div className={styles.inputContainer}>
-                <span>Nombre</span>
-                <input type="text" name='nombre' value={form["nombre"]} 
-              onChange={handleChange}/>
-            </div>
-            </form>
-            <button 
-            className={styles.sendBtn} onClick={handleSubmit} 
-            disabled={!form["nombre"]}>
-              Enviar
-            </button>
+        <ToastContainer />
+        {(isLoading || isLoadingSucursales) && (
+          <div className={styles.spinnerOverlay}>
+            <ClipLoader
+              size={60}
+              color="#800020" // bordó
+              loading={true}
+            />
+            <p className={styles.loadingText}>Cargando...</p>
+          </div>
+        )}
+        <h2>Sucursales</h2>
+        <button onClick={handleActualizar} className={styles.refreshButton}>
+          🔄 Actualizar
+        </button>
+        <DataGrid
+          className={styles.dataGrid}
+          dataSource={sucursales || []}
+          showBorders={true}
+          style={{ fontFamily: "IBM" }}
+          rowAlternationEnabled={true}
+          allowColumnResizing={true}
+          height={300}
+          columnAutoWidth={true}>
+          <Column dataField="nombre" caption="Nombre" />
+          <Column dataField="id" width={100} caption="" alignment="center" cellRender={renderModificarCell} />
+        </DataGrid>
+        <h2>Alta de sucursal</h2>
+        <form action="" enctype="multipart/form-data" className={styles.form}>
+          <div className={styles.inputContainer}>
+            <span>Nombre</span>
+            <input type="text" name='nombre' value={form["nombre"]}
+              onChange={handleChange} />
+          </div>
+        </form>
+        <button
+          className={styles.sendBtn} onClick={handleSubmit}
+          disabled={!form["nombre"]}>
+          Enviar
+        </button>
       </div>
     </div>
   )
