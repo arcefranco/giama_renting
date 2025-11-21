55e75e186a3f772c1ff38c947e6d3ede1e45f421
franco <arcefranco29@gmail.com>
2025-11-21 09:56:27 -0300

    fix getRecibosById

--- backend/src/routes/recibosRoutes.js
+++ backend/src/routes/recibosRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.js";
-import { anulacionRecibo, getReciboById, getRecibos } from "../controllers/recibosController.js";
+import { anulacionRecibo, getReciboById, getRecibos, getRecibosByFormaCobro } from "../controllers/recibosController.js";
 import { authorizeRoles } from "../middlewares/roles.js";
 const recibosRouter = Router();
 recibosRouter.use((req, res, next) => {
@@ -13,5 +13,6 @@ recibosRouter.use((req, res, next) => {
 
 recibosRouter.post("/getReciboById", auth, authorizeRoles("5"), getReciboById);
 recibosRouter.get("/getRecibos", auth, authorizeRoles("5"), getRecibos);
+recibosRouter.post("/getRecibosByFormaCobro", auth, authorizeRoles("5"), getRecibosByFormaCobro);
 recibosRouter.post("/anulacionRecibo", auth, authorizeRoles("2"), anulacionRecibo)
 export default recibosRouter;
