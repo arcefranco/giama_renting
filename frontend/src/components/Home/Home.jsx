f10483980f78104d72cf7213466a252ff3e3e82d
franco <arcefranco29@gmail.com>
2025-12-03 12:03:54 -0300

    home

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 const Home = () => {
-  const {roles, nombre, username} = useSelector((state) => state.loginReducer)
-  
+  const { roles, nombre, username } = useSelector((state) => state.loginReducer)
+
   return (
     <div>
-        <h1>Bienvenido/a {nombre} {"(testing)"}</h1>
+      <h1>Bienvenido/a {nombre}</h1>
     </div>
   )
 }
